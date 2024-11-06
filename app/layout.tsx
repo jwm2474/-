a3f8784797e262bed62207d8d23a7bf05dc3949c
jwm2474 <105188294+jwm2474@users.noreply.github.com>
2024-11-06 17:15:56 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
 import { getLocaleOnServer } from '@/i18n/server'
-
 import './styles/globals.css'
 import './styles/markdown.scss'
 
-const LocaleLayout = ({
-  children,
-}: {
-  children: React.ReactNode
-}) => {
+const LocaleLayout = ({ children }: { children: React.ReactNode }) => {
   const locale = getLocaleOnServer()
   return (
     <html lang={locale ?? 'en'} className="h-full">
@@ -17,9 +12,13 @@ const LocaleLayout = ({
             {children}
           </div>
         </div>
+        <footer className="footer">
+          <p>服务生成的所有内容均由人工智能模型生成，其生成内容的准确性和完整性无法保证，不代表辽宁工程技术大学的态度或观点</p>
+        </footer>
       </body>
     </html>
   )
 }
 
 export default LocaleLayout
+
